fc76418653dbf5e30a0b42528d27396adb4452cc
Rancic <lukinator28@gmail.com>
2024-07-17 10:43:19 +0200

    feat: Add support for relative paths

--- src/load.rs
+++ src/load.rs
@@ -1,10 +1,16 @@
-use std::path::PathBuf;
+use std::{env::current_dir, path::PathBuf};
 
 use walkdir::WalkDir;
 
 const MUSIC_FILE_EXTENSIONS: [&str; 3] = ["wav", "ogg", "mp3"];
 
 pub fn load_music_files(path: &PathBuf) -> Vec<std::path::PathBuf> {
+    let path = if path.is_relative() {
+        &current_dir().unwrap().join(path)
+    } else {
+        path
+    };
+
     let mut music_files = Vec::new();
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
         let path = entry.path();
